ece77582570aa9fba50f915b5709e62f87294105
karanindulkar <karanindulkar8@gmail.com>
2025-06-27 11:13:14 -0700

    Initial-design

---
+++ password_generator/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".btn");
+const lowercase = document.querySelector(".options .low");
+
+
+btn.addEventListener("click", function(){
+    console.log('I was clicked');  
+})
+
+lowercase.addEventListener("click", function(){
+    console.log('Lowercase was selected');  
+})
